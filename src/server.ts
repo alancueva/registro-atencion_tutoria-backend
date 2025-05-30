fe56411098bfa1c0c4c27b8363c4e5dd530f2831
alancuevat <alancuevat@gmail.com>
2025-05-30 16:25:12 -0500

    cambio en la consola de ingles a español

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ import config from './config/config';
 const PORT = config.app.port;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Servidor está corriendo en el puerto:  ${PORT}`);
 });
