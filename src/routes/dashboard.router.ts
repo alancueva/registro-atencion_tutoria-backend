6055081a35652014e08a52d6345307f2b2f56ee4
alancuevat <alancuevat@gmail.com>
2025-06-05 11:56:07 -0500

    fix: corregir ruta de endpoint para ruta de docente_sesiones

--- src/routes/dashboard.router.ts
+++ src/routes/dashboard.router.ts
@@ -7,6 +7,6 @@ const dashboardController = new DashboardController();
 router.get("/dashboard", dashboardController.getDashboardData.bind(dashboardController));
 router.get("/dashboard/cantidad_sesiones", dashboardController.getDashboardCantidadSesiones.bind(dashboardController));
 router.get("/dashboard/conteo_porcentaje_area", dashboardController.getDashboardConteoPorcentajeArea.bind(dashboardController));
-router.get("/dashboard/docente_sesiones/:idusuario", dashboardController.getDashboardDocenteSesiones.bind(dashboardController));
+router.get("/docente_sesiones/:idusuario", dashboardController.getDashboardDocenteSesiones.bind(dashboardController));
 
 export default router;
