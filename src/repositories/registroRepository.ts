574f52bf117eed289ad8d7e064e38a7723ecfd09
alancuevat <alancuevat@gmail.com>
2025-06-07 19:30:15 -0500

    fix: correccion en la busqueda de administrador (getRegistrosBusqueda)

--- src/repositories/registroRepository.ts
+++ src/repositories/registroRepository.ts
@@ -3,74 +3,73 @@ import { Registro, RegistroBusqueda, RegistroBusquedaDocente, RegistroResponse }
 
 export class RegistroRepository {
 
-    public async getRegistrosBusquedaDocente(registroDTO: RegistroBusquedaDocente):Promise<RegistroResponse[]> {    
+    public async getRegistrosBusquedaDocente(registroDTO: RegistroBusquedaDocente): Promise<RegistroResponse[]> {
         try {
             const [rows]: any = await pool.query("CALL sp_buscar_tabla_docente(?,?,?,?,?)",
                 [registroDTO.id_usuario,
-                    registroDTO.anio,
-                    registroDTO.mes,
-                    `%${registroDTO.area}%`, 
-                    `%${registroDTO.atencion}%` 
-                    ]);
-            
+                registroDTO.anio,
+                registroDTO.mes,
+                `%${registroDTO.area}%`,
+                `%${registroDTO.atencion}%`
+                ]);
+
             return rows[0] as RegistroResponse[];
-            
+
         } catch (error) {
-            
+
             console.error(error);
             throw new Error('Error fetching registros');
         }
     }
 
-    public async getRegistrosBusqueda(registroDTO: RegistroBusqueda):Promise<RegistroResponse[]> {    
+    public async getRegistrosBusqueda(registroDTO: RegistroBusqueda): Promise<RegistroResponse[]> {
         try {
             const [rows]: any = await pool.query("CALL sp_buscar_tabla_admin(?,?,?,?,?)",
                 [registroDTO.anio,
-                    registroDTO.mes,
-                    `%${registroDTO.programa}%`, 
-                    `%${registroDTO.area}%`, 
-                    `%${registroDTO.atencion}%`]);
-            
+                registroDTO.mes,
+                registroDTO.programa,
+                registroDTO.area,
+                registroDTO.atencion]);
             return rows[0] as RegistroResponse[];
-            
+
         } catch (error) {
-            
+
             console.error(error);
             throw new Error('Error fetching registros');
         }
     }
 
-    public async insert_tabla(registroDTO: Registro): Promise<boolean> {   
+    public async insert_tabla(registroDTO: Registro): Promise<boolean> {
         try {
             await pool.query("CALL sp_tabla_insert(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
                 [registroDTO.nom_ape,
-                    registroDTO.idarea,
-                    registroDTO.motivo,
-                    registroDTO.osb,
-                    registroDTO.fecha,
-                    registroDTO.idatencion,
-                    registroDTO.programa,
-                    registroDTO.turno,
-                    registroDTO.semestre,
-                    registroDTO.tutor,
-                    registroDTO.idusuario,
-                    registroDTO.periodo_academico,
-                    registroDTO.anio,
-                    registroDTO.vigencia,
-                    registroDTO.usuario_creacion,
-                    registroDTO.fecha_creacion,
-                    registroDTO.usuario_modificacion,
-                    registroDTO.fecha_modificacion]);
-            
+                registroDTO.idarea,
+                registroDTO.motivo,
+                registroDTO.osb,
+                registroDTO.fecha,
+                registroDTO.idatencion,
+                registroDTO.programa,
+                registroDTO.turno,
+                registroDTO.semestre,
+                registroDTO.tutor,
+                registroDTO.idusuario,
+                registroDTO.periodo_academico,
+                registroDTO.anio,
+                registroDTO.vigencia,
+                registroDTO.usuario_creacion,
+                registroDTO.fecha_creacion,
+                registroDTO.usuario_modificacion,
+                registroDTO.fecha_modificacion]);
+
             return true;
-            
+
         } catch (error) {
-            
+
             console.error(error);
             throw new Error('Error inserting registros');
         }
-    }  
-    
+    }
+
     public async insertTablaMultiple(jsonData: any[]): Promise<boolean> {
         try {
             await pool.query("CALL sp_tabla_insert_multiple(?)", [JSON.stringify(jsonData)]);
